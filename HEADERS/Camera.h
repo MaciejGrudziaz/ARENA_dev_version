ed78b34361e98f4ba7af56d1abda5b7290c6fcc0
MaciejGrudziaz <maciejgrudziaz@gmail.com>
2018-05-17 11:47:40 +0200

    Camera update

--- HEADERS/Camera.h
+++ HEADERS/Camera.h
@@ -2,6 +2,7 @@
 #include <windows.h>
 #include <gl/glu.h>
 #include "Vector.h"
+#include "DynamicCharacter.h"
 
 //------------------------------------------------------------------------------------------------------
 //------------------------------------------------KAMERA------------------------------------------------
@@ -9,35 +10,46 @@
 //------------------------------------------------------------------------------------------------------
 
 class Camera {
-	static Vector position;									//pozycja kamery
-	static Vector orientation;								//orientacja kamery
-	static Vector center;									//pozycja punktu w kierunku którym 'patrzy' kamera
-	static Vector upvector;									//wektor wskazuj¹cy 'górê' kamery
+	Vector position;									//pozycja kamery
+	Vector orientation;								//orientacja kamery
+	Vector center;									//pozycja punktu w kierunku którym 'patrzy' kamera
+	Vector upvector;									//wektor wskazuj¹cy 'górê' kamery
 
-	static Vector relativePos;								//pozycja kamery wzglêdem postaci gracza
-	static Vector relativeCenter;							//pozycja punktu w kierunku którym 'patrzy' kamera wzglêdem postaci gracza
+	Vector relativePos;								//pozycja kamery wzglêdem postaci gracza
+	Vector relativeCenter;							//pozycja punktu w kierunku którym 'patrzy' kamera wzglêdem postaci gracza
+
+	enum Mode{FREE,BOUND};
+	Mode mode;
+
+	DynamicCharacter* character;
 
 public:
+	Camera();
+	Camera(Vector Position, Vector Center, Vector upVector, Mode mode_);
+	Camera(double xPos, double yPos, double zPos, double xCen, double yCen, double zCen, double xUpVec, double yUpVec, double zUpVec, Mode mode_);
+
 	//inicjalizacja kamery przy pomocy wektorów
-	static void Initialize(Vector Position, Vector Center, Vector upVector);
+	void Initialize(Vector Position, Vector Center, Vector upVector,Mode mode_);
 	//inicjalizacja kamery przy pomocy zmiennych opisuj¹cych ka¿d¹ wspó³rzêdn¹
-	static void Initialize(double xPos, double yPos, double zPos, double xCen, double yCen, double zCen, double xUpVec, double yUpVec, double zUpVec);
+	void Initialize(double xPos, double yPos, double zPos, double xCen, double yCen, double zCen, double xUpVec, double yUpVec, double zUpVec,Mode mode_);
+	//inicjalizacja postaci przypisanej do kamery
+	void InitializeCharacter(DynamicCharacter& character_) { character = &character_; }
 	//ustawienie kamery
-	static void SetCamera();
+	void SetCamera();
 
 	//funkcje ustawiaj¹ce odpowiednie wartoœci dla wpsó³rzêdnych kamery 
-	static void SetPosition(double x, double y, double z) { position.x = x; position.y = y; position.z = z; }
-	static void SetOrientation(double x, double y, double z) { orientation.x = x; orientation.y = y; orientation.z = z; }
-	static void SetCenter(double x, double y, double z) { center.x = x; center.y = y; center.z = z; }
-	static void SetUpVector(double x, double y, double z) { upvector.x = x; upvector.y = y; upvector.z = z; }
-	static void SetRelativePosition(double x, double y, double z) { relativePos.x = x; relativePos.y = y; relativePos.z = z; }
-	static void SetRelativeCenter(double x, double y, double z) { relativeCenter.x = x; relativeCenter.y = y; relativeCenter.z = z; }
+	void SetPosition(double x, double y, double z) { position.x = x; position.y = y; position.z = z; }
+	void SetOrientation(double x, double y, double z) { orientation.x = x; orientation.y = y; orientation.z = z; }
+	void SetCenter(double x, double y, double z) { center.x = x; center.y = y; center.z = z; }
+	void SetUpVector(double x, double y, double z) { upvector.x = x; upvector.y = y; upvector.z = z; }
+	void SetRelativePosition(double x, double y, double z) { relativePos.x = x; relativePos.y = y; relativePos.z = z; }
+	void SetRelativeCenter(double x, double y, double z) { relativeCenter.x = x; relativeCenter.y = y; relativeCenter.z = z; }
 
 	//funkcje pobieraj¹ce odpowiednie wartoœci wspó³rzêdnych kamery
-	static Vector GetPosition() { return position; }
-	static Vector GetOrientation() { return orientation; }
-	static Vector GetCenter() { return center; }
-	static Vector GetUpVector() { return upvector; }
-	static Vector GetRelativePosition() { return relativePos; }
-	static Vector GetRelativeCenter() { return relativeCenter; }
+	Vector GetPosition() const { return position; }
+	Vector GetOrientation() const { return orientation; }
+	Vector GetCenter() const { return center; }
+	Vector GetUpVector() const { return upvector; }
+	Vector GetRelativePosition() const { return relativePos; }
+	Vector GetRelativeCenter() const { return relativeCenter; }
 };
--- SRC/Camera.cpp
+++ SRC/Camera.cpp
@@ -1,23 +1,51 @@
 #include "Camera.h"
 
 //inicjalizacja sk³adowych staycznych klasy
-Vector Camera::position = { 0.0, 0.0, 0.0 };
-Vector Camera::orientation = { 0.0,0.0,0.0 };
-Vector Camera::center = { 0.0, 0.0, -10.0 };
-Vector Camera::upvector = { 0.0, 0.0, 0.0 };
-Vector Camera::relativePos = { 0.0,0.0,0.0 };
-Vector Camera::relativeCenter = { 0.0,0.0,0.0 };
+//Vector Camera::position = { 0.0, 0.0, 0.0 };
+//Vector Camera::orientation = { 0.0,0.0,0.0 };
+//Vector Camera::center = { 0.0, 0.0, -10.0 };
+//Vector Camera::upvector = { 0.0, 0.0, 0.0 };
+//Vector Camera::relativePos = { 0.0,0.0,0.0 };
+//Vector Camera::relativeCenter = { 0.0,0.0,0.0 };
+
+Camera::Camera() {
+	position = { 0.0, 0.0, 0.0 };
+	orientation = { 0.0,0.0,0.0 };
+	center = { 0.0, 0.0, 0.0 };
+	upvector = { 0.0, 0.0, 0.0 };
+	relativePos = { 0.0,0.0,0.0 };
+	relativeCenter = { 0.0,0.0,0.0 };
+	mode = FREE;
+	character = nullptr;
+}
+
+Camera::Camera(Vector Position, Vector Center, Vector upVector, Mode mode_){
+	position = Position;
+	center = Center;
+	upvector = upVector;
+	mode = mode_;
+	character = nullptr;
+}
+
+Camera::Camera(double xPos, double yPos, double zPos, double xCen, double yCen, double zCen, double xUpVec, double yUpVec, double zUpVec, Mode mode_) {
+	position = { xPos,yPos,zPos };
+	center = { xCen,yCen,zCen };
+	upvector = { xUpVec,yUpVec,zUpVec };
+	mode = mode_;
+	character = nullptr;
+}
 
 //inicjalizacja kamery przy pomocy wektorów
-void Camera::Initialize(Vector Position, Vector Center, Vector UpVector)
+void Camera::Initialize(Vector Position, Vector Center, Vector UpVector, Mode mode_)
 {
 	position = Position;
 	center = Center;
 	upvector = UpVector;
+	mode = mode_;
 }
 
 //inicjalizacja kamery przy pomocy zmiennych opisuj¹cych dane wspó³rzêdne
-void Camera::Initialize(double xPos, double yPos, double zPos, double xCen, double yCen, double zCen, double xUpVec, double yUpVec, double zUpVec)
+void Camera::Initialize(double xPos, double yPos, double zPos, double xCen, double yCen, double zCen, double xUpVec, double yUpVec, double zUpVec, Mode mode_)
 {
 	position.x = xPos;
 	position.y = yPos;
@@ -30,6 +58,8 @@ void Camera::Initialize(double xPos, double yPos, double zPos, double xCen, doub
 	upvector.x = xUpVec;
 	upvector.y = yUpVec;
 	upvector.z = zUpVec;
+
+	mode = mode_;
 }
 
 //ustawienie kamery w scenie OpenGL
